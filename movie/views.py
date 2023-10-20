69cd10c0b76c1a0cf6a9539c2f38dcfaacdfcd79
dsbetancu1 <dsbetancu1@eafit.edu.co>
2023-10-20 17:05:56 -0500

    Login

--- movie/views.py
+++ movie/views.py
@@ -1,18 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from django.http import HttpResponse
 
 from .models import Movie
-# Create your views here.
 
-def home(request):
-    #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    #return render (request, 'home.html')
+def home(request, username=None):
     searchTerm = request.GET.get('searchMovie')
     if searchTerm:
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies, 'username': username})
 
-def about(request):
-    return render(request, 'about.html',{'name':'Daniel Betancur'})
+def about(request, username=None):
+    return render(request, 'about.html', {'name': username})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        return redirect('home', username=username)
+    return render(request, 'login.html')
+
+
